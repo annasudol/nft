ec8e29cc587c1b62285a0eeebf6bd30cf7f3e9e3
anna <anjasudol@gmail.com>
2022-07-22 11:47:23 +0100

    account

--- components/hooks/web3/useAccount.ts
+++ components/hooks/web3/useAccount.ts
@@ -7,8 +7,14 @@ type AccountHookFactory = CryptoHookFactory<string, string>
 export type UseAccountHook = ReturnType<AccountHookFactory>
 
 export const hookFactory: AccountHookFactory = ({provider}) => (params) => {
-  const swrRes =   useSWR(provider ? "web3/useAccount" : null, ()=> {
-    return "test"
+  const swrRes =   useSWR(provider ? "web3/useAccount" : null, async()=> {
+    const accounts = await provider!.listAccounts();
+    const account =  accounts[0];
+    console.log(account);
+    if (!account) {
+      throw "Cannot retrieve account! Connect to wallet"
+    }
+    return account;
   });
   return swrRes;
 }
