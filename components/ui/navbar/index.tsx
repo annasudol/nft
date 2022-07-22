586dbaa2d1beb1e9e6cfcbe81c33c81443b9b753
anna <anjasudol@gmail.com>
2022-07-22 10:40:17 +0100

    types

--- components/ui/navbar/index.tsx
+++ components/ui/navbar/index.tsx
@@ -87,7 +87,7 @@ export default function Navbar() {
                   </div>
                   <Menu.Items className="origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg py-1 bg-white ring-1 ring-black ring-opacity-5 focus:outline-none">
                     <Menu.Item>
-                      {({ active }) => (
+                      {({ active }:{active: boolean}) => (
                         <Link href="/profile">
                           <a
                             className={classNames(active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700')}
